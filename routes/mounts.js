8c5f5a938e9f3d5877e7ad2dc8ee2abf6d008a46
arman nugraha <armannugraha85@gmail.com>
2020-05-23 20:27:02 +0700

    add paginate in mount

--- routes/mounts.js
+++ routes/mounts.js
@@ -11,14 +11,46 @@ const operatorsAliases = {
 }
 var Mount = require('../models').mounts
 var view = require('../views')
+const pageLimit = 10
+
+//PAGINATION Function
+const paginate = (query, { page, pageSize }) => {
+  // const offset = page * pageSize;
+  const limit = pageLimit;
+  const offset = 0 + (page - 1) * limit;
+  return {
+    ...query,
+    offset,
+    limit,
+  };
+};
 
 router.get('/', async function (req, res, next) {
-  const mounts = await Mount.findAll({})
-  if (mounts.length !== 0) {
-    res.json(view(mounts))
-  } else {
-    res.json(view('mounts empty'))
-  }
+    const count_mount = await Mount.count({})
+    const totalPage = Math.ceil(count_mount/pageLimit)
+
+    let page = +req.query.page;
+    let pageSize = count_mount;
+    const mounts = await Mount.findAll(paginate(
+        {
+            where: {}, // conditions
+            order: [
+                ['id', 'DESC']
+            ]
+        },
+        { page, pageSize },
+    ))
+
+    if (mounts.length !== 0) {
+        res.status(200).json({
+            'status': 'ok',
+            'pageSize': totalPage,
+            'data': mounts,
+        })
+    } else {
+        res.json(view('mounts empty'))
+    }
+
 })
 
 router.get('/:id', async (req, res, next) => {
