d7c9aee75d41cdcbf37d71fb0206894c4fd1c51b
arman nugraha <armannugraha85@gmail.com>
2020-06-12 14:53:27 +0700

    add seed item list

--- seeders/20200529084206-gear_items.js
+++ seeders/20200529084206-gear_items.js
@@ -16,6 +16,8 @@ module.exports = {
         //kalo bisa ambil suhu pada saat malam hari
         {gear_id:2,name: 'Onsight Sleeping Bag Mummy Extreem Peak 05',weight:1500,condition:0,type:0,capacity: "Panjang 205cm"},
         {gear_id:2,name: 'Deuter Sleeping Bag Dreamlite L500',weight:700,condition:1,type:1,capacity: "Panjang 220cm"},
+        {gear_id:2,name: 'Arei AG Sleeping Bag',weight:900,condition:1,type:0,capacity: "Panjang 210cm"},
+        {gear_id:2,name: 'Eiger Sleeping Bag',weight:500,condition:0,type:1,capacity: "Panjang 205cm"},
 
         {gear_id:3,name: 'Aether AG 70',weight:2500,condition:1,type:0,capacity: "70 Liter"},
         {gear_id:3,name: 'Kestrel 48',weight:1700,condition:1,type:0,capacity: "48 Liter"},
@@ -36,10 +38,13 @@ module.exports = {
         {gear_id:6,name: 'Flysheet 2x3',weight:600,condition:1,type:1,capacity:"2x3"},
         {gear_id:6,name: 'Flysheet 3x4',weight:700,condition:1,type:0,capacity:"3x4"},
         {gear_id:6,name: 'Flysheet Ultralight 2x3',weight:500,condition:0,type:1,capacity:"2x3"},
+        {gear_id:6,name: 'Flysheet 4x4',weight:500,condition:0,type:0,capacity:"4x4"},
 
         {gear_id:7,name: 'Hike Light 1.1 Jacket',weight:550,condition:0,type:1,capacity:"Suhu 15-20 C"},
         {gear_id:7,name: 'Finder Insulation Jacket',weight:1000,condition:1,type:0,capacity:"Suhu 10-15 C"},
         {gear_id:7,name: 'Fordate 1.0 X Jacket',weight:400,condition:0,type:1,capacity:"Suhu 15-20 C"},
+        {gear_id:7,name: 'Fordate 2.0 X Jacket',weight:600,condition:1,type:1,capacity:"Suhu 5-10 C"},
+        {gear_id:7,name: 'Naturhike Jacket',weight:800,condition:0,type:0,capacity:"Suhu 0-15 C"},
         
         {gear_id:8,name: 'ASV Rubber Raincoat',weight:1750,condition:1,type:0,capacity:"Karet Sintetis"},
         {gear_id:8,name: 'Arei Raincoat Stormshield',weight:800,condition:1,type:0,capacity:"PVC Rubber"},
@@ -51,25 +56,32 @@ module.exports = {
         {gear_id:9,name: 'Eiger Shoes Boot Pollock',weight:1000,condition:1,type:0,capacity:"Size 38-45"},
         
         {gear_id:10,name: 'Men Tactical Gloves',weight:500,condition:1,type:0,capacity:"Size M-XL"},
+        {gear_id:10,name: 'Men Tactical Gloves',weight:500,condition:0,type:0,capacity:"Size M-XL"},
         {gear_id:10,name: 'Outdoor Gloves',weight:100,condition:1,type:1,capacity:"Size M-XL"},
+        {gear_id:10,name: 'Outdoor Gloves',weight:100,condition:0,type:1,capacity:"Size M-XL"},
         
         {gear_id:11,name: 'Lampu Senter Emergency Lentera',weight:250,condition:0,type:0,capacity:"300 Lumen"},
         {gear_id:11,name: 'Cree Flashlight C8',weight:250,condition:1,type:0,capacity:"3800 Lumen"},
         {gear_id:11,name: 'Pocketman LED Flashlight',weight:60,condition:1,type:1,capacity:"2000 Lumen"},
+        {gear_id:11,name: 'Pocketman LED Flashlight',weight:60,condition:0,type:1,capacity:"2000 Lumen"},
         
         {gear_id:12,name: 'Kompor spirtus gel',weight:100,condition:0,type:1,capacity:"Spirtus"},
         {gear_id:12,name: 'Kompor trangia',weight:200,condition:0,type:0,capacity:"Spirtus"},
         {gear_id:12,name: 'Kompor mini wind prof',weight:300,condition:0,type:1,capacity:"Tabung Hicook"},
+        {gear_id:12,name: 'Kompor mini wind prof',weight:300,condition:1,type:1,capacity:"Tabung Hicook"},
         {gear_id:12,name: 'Niko Kompor Portable NK – 268',weight:1000,condition:0,type:0,capacity:"Tabung Hicook"},
+        {gear_id:12,name: 'Niko Kompor Portable NK – 268',weight:1000,condition:1,type:0,capacity:"Tabung Hicook"},
         {gear_id:12,name: 'Kompor ultralight',weight:80,condition:0,type:1,capacity:"Tabung Hicook"},
         
-        {gear_id:13,name: 'Army Nesting Set',weight:100,condition1:,type:1,capacity:"2 Orang"},
+        {gear_id:13,name: 'Army Nesting Set',weight:100,condition:1,type:1,capacity:"2 Orang"},
         {gear_id:13,name: 'Camping Frying Pan',weight:50,condition:1,type:1,capacity:"2 Orang"},
         {gear_id:13,name: 'Cookware Set',weight:200,condition:1,type:1,capacity:"4 Orang"},
         {gear_id:13,name: '3-in-1 Pot Set',weight:500,condition:1,type:0,capacity:"6-8 Orang"},
 
         {gear_id:14,name: 'Tali Karmantel Statis',weight:5000,condition:1,type:0,capacity:"P=50m, L=10,5mm, Beban=250kg"},
+        {gear_id:14,name: 'Tali Karmantel Statis',weight:5000,condition:0,type:0,capacity:"P=50m, L=10,5mm, Beban=250kg"},
         {gear_id:14,name: 'Tali Webbing Sling Tubular',weight:200,condition:1,type:1,capacity:"P=5-45m, L=2,5cm, Beban=250kg"},
+        {gear_id:14,name: 'Tali Webbing Sling Tubular',weight:200,condition:0,type:1,capacity:"P=5-45m, L=2,5cm, Beban=250kg"},
         
         // {gear_id:15,name: '',weight:,condition:,type:,capacity:""},
         // {gear_id:15,name: '',weight:,condition:,type:,capacity:""},
