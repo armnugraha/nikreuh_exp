42dc0c2ee3fddfee4f6431659d459c32197fc4a0
Aar <armannugraha85@gmail.com>
2020-07-06 20:37:51 +0700

    modify seed

--- routes/seeds.js
+++ routes/seeds.js
@@ -16,11 +16,14 @@ router.get('/create/seed/auto', async (req, res, next) => {
             var data = [1,2,3,4]
             var iM = Math.floor(4*Math.random())
 
+            var data_gender = [0,1]
+            var iG = Math.floor(2*Math.random())
+
             var data_rate = [1,2,3,4,5]
             var iR = Math.floor(5*Math.random())
 
             const users = await Models.users.create({
-                username:"data_testing"+i,name:"Data Testing"+i,email:"data_testing"+i+"@gmail.com",password: bcrypt.hashSync("password", bcrypt.genSaltSync(10)),phone:87822516+i,gender:0,role_id:3
+                username:"data_testing"+i,name:"Data Testing"+i,email:"data_testing"+i+"@gmail.com",password: bcrypt.hashSync("password", bcrypt.genSaltSync(10)),phone:87822516+i,gender:data_gender[iG],role_id:3
             });
             const reviews = await Review.create({mount_id:data[iM],user_id:i,rate:data_rate[iR],desc:"Data Testing"+i});
         }
