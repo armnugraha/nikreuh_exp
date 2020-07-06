06165db09c4bd864e18e504c4637dab3cb94f869
Aar <armannugraha85@gmail.com>
2020-07-06 21:37:51 +0700

    modify seed

--- routes/seeds.js
+++ routes/seeds.js
@@ -11,7 +11,7 @@ const pageLimit = 8
 
 router.get('/create/seed/auto', async (req, res, next) => {
     try {
-        for (var i = 6; i <= 600; i++) {
+        for (var i = 601; i <= 1200; i++) {
 
             var data = [1,2,3,4]
             var iM = Math.floor(4*Math.random())
@@ -19,8 +19,8 @@ router.get('/create/seed/auto', async (req, res, next) => {
             var data_gender = [0,1]
             var iG = Math.floor(2*Math.random())
 
-            var data_rate = [1,2,3,4,5]
-            var iR = Math.floor(5*Math.random())
+            var data_rate = [3,4,5]
+            var iR = Math.floor(3*Math.random())
 
             const users = await Models.users.create({
                 username:"data_testing"+i,name:"Data Testing"+i,email:"data_testing"+i+"@gmail.com",password: bcrypt.hashSync("password", bcrypt.genSaltSync(10)),phone:87822516+i,gender:data_gender[iG],role_id:3
--- seeders/20200313080537-user-seeder.js
+++ seeders/20200313080537-user-seeder.js
@@ -4,54 +4,55 @@ const bcrypt = require("bcrypt");
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('users', [{
-      name: 'Super User',
-      username: 'super_user',
-      email: 'superuser@unikom.com',
-      password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
-      gender:0,
-      birth:"1998-05-19",
-      role_id: 1
-    },{
-      name: 'Aan',
-      username: 'admin_manglayang',
-      email: 'admin@manglayang.com',
-      password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
-      phone:('0' + 87812316625).slice(-12),
-      gender:0,
-      birth:"1980-05-19",
-      role_id: 2
-    },
-    {
-      name: 'Kostaman',
-      username: 'admin_cilengkrang',
-      email: 'admin_curug@cilengkrang.com',
-      password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
-      phone:('0' + 87812316626).slice(-12),
-      gender:0,
-      birth:"1982-05-19",
-      role_id: 2
-    },
-    {
-      name: 'Admin Jayagiri',
-      username: 'admin_jayagiri',
-      email: 'admin@jayagiri.com',
-      password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
-      phone:('0' + 87812316627).slice(-12),
-      gender:0,
-      birth:"1986-05-19",
-      role_id: 2
-    },
-    {
-      name: 'Admin CIC',
-      username: 'admin_cic',
-      email: 'admin@cic.com',
-      password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
-      phone:('0' + 87812316628).slice(-12),
-      gender:0,
-      birth:"1989-05-19",
-      role_id: 2
-    },
+    return queryInterface.bulkInsert('users', [
+    // {
+    //   name: 'Super User',
+    //   username: 'super_user',
+    //   email: 'superuser@unikom.com',
+    //   password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
+    //   gender:0,
+    //   birth:"1998-05-19",
+    //   role_id: 1
+    // },{
+    //   name: 'Aan',
+    //   username: 'admin_manglayang',
+    //   email: 'admin@manglayang.com',
+    //   password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
+    //   phone:('0' + 87812316625).slice(-12),
+    //   gender:0,
+    //   birth:"1980-05-19",
+    //   role_id: 2
+    // },
+    // {
+    //   name: 'Kostaman',
+    //   username: 'admin_cilengkrang',
+    //   email: 'admin_curug@cilengkrang.com',
+    //   password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
+    //   phone:('0' + 87812316626).slice(-12),
+    //   gender:0,
+    //   birth:"1982-05-19",
+    //   role_id: 2
+    // },
+    // {
+    //   name: 'Admin Jayagiri',
+    //   username: 'admin_jayagiri',
+    //   email: 'admin@jayagiri.com',
+    //   password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
+    //   phone:('0' + 87812316627).slice(-12),
+    //   gender:0,
+    //   birth:"1986-05-19",
+    //   role_id: 2
+    // },
+    // {
+    //   name: 'Admin CIC',
+    //   username: 'admin_cic',
+    //   email: 'admin@cic.com',
+    //   password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
+    //   phone:('0' + 87812316628).slice(-12),
+    //   gender:0,
+    //   birth:"1989-05-19",
+    //   role_id: 2
+    // },
     // {
     //   name: 'Admin Sewa',
     //   username: 'admin_sewa',
@@ -62,76 +63,69 @@ module.exports = {
   
     //   role_id: 3
     // },
-    // {
-    //   name: 'arman',
-    //   username: 'arman',
-    //   email: 'arman@unikom.com',
-    //   password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
-    //   gender:0,
-    //   birth:"1998-05-19",
-  
-    //   role_id: 3
-    // },
-    // {
-    //   name: 'shiffa',
-    //   username: 'shiffa',
-    //   email: 'shiffa@unikom.com',
-    //   password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
-    //   gender:1,
-    //   birth:"1998-06-17",
-  
-    //   role_id: 3
-    // },
-    // {
-    //   name: 'rikad',
-    //   username: 'rikad',
-    //   email: 'rikad@unikom.com',
-    //   password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
-    //   gender:0,
-    //   birth:"1997-02-19",
-  
-    //   role_id: 3
-    // },
-    // {
-    //   name: 'indra',
-    //   username: 'indra',
-    //   email: 'indra@unikom.com',
-    //   password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
-    //   gender:0,
-    //   birth:"1997-10-19",
-  
-    //   role_id: 3
-    // },
-    // {
-    //   name: 'faishal',
-    //   username: 'faishal',
-    //   email: 'faishal@unikom.com',
-    //   password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
-    //   gender:0,
-    //   birth:"1998-05-19",
-  
-    //   role_id: 3
-    // },
-    // {
-    //   name: 'heruwin',
-    //   username: 'heruwin',
-    //   email: 'heruwin@unikom.com',
-    //   password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
-    //   gender:0,
-    //   birth:"1998-05-19",
-  
-    //   role_id: 3
-    // },
-    // {
-    //   name: 'ihan',
-    //   username: 'ihan',
-    //   email: 'ihan@unikom.com',
-    //   password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
-    //   gender:0,
-    //   birth:"1998-05-19",
-  
-    //   role_id: 3
-    // }
+    {
+      name: 'arman',
+      username: 'arman',
+      email: 'arman@unikom.com',
+      password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
+      gender:0,
+      birth:"1998-05-19",
+      role_id: 3
+    },
+    {
+      name: 'lestari',
+      username: 'lestari',
+      email: 'lestari@unikom.com',
+      password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
+      gender:1,
+      birth:"1998-06-17",
+      role_id: 3
+    },
+    {
+      name: 'rikad',
+      username: 'rikad',
+      email: 'rikad@unikom.com',
+      password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
+      gender:0,
+      birth:"1997-02-19",
+      role_id: 3
+    },
+    {
+      name: 'indra',
+      username: 'indra',
+      email: 'indra@unikom.com',
+      password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
+      gender:0,
+      birth:"1997-10-19",
+      role_id: 3
+    },
+    {
+      name: 'faishal',
+      username: 'faishal',
+      email: 'faishal@unikom.com',
+      password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
+      gender:0,
+      birth:"1998-05-19",
+      role_id: 3
+    },
+    {
+      name: 'heruwin',
+      username: 'heruwin',
+      email: 'heruwin@unikom.com',
+      password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
+      gender:0,
+      birth:"1998-05-19",
+      role_id: 3
+    },
+    {
+      name: 'ihan',
+      username: 'ihan',
+      email: 'ihan@unikom.com',
+      password: bcrypt.hashSync('password', bcrypt.genSaltSync(10)),
+      gender:0,
+      birth:"1998-05-19",
+      role_id: 3
+    }
     ], {})
   },
 
